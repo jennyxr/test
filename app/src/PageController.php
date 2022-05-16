f8230329303e6dcb5c0f54901515de6a68693abb
jennyxr <103478433+jennyxr@users.noreply.github.com>
2022-05-16 13:58:36 +1200

    Update PageController.php

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -28,6 +28,8 @@ namespace {
             parent::init();
             // You can include any CSS or JS required by your project here.
             // See: https://docs.silverstripe.org/en/developer_guides/templates/requirements/
+            
+            var_dump("Test HERE");
         }
     }
 }
